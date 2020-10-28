39a3172f30c79b4a06bb1d0a64df9279dd09cf26
fastsly <szalai.laszlo.istvan.psiho@gmail.com>
2020-10-28 11:32:00 +0200

    bound port correctly

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post("/daily", (req, res) => {
     handleDatabaseInsert(req.body, res);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running!");
 });
 
