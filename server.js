2d455fe937d4abc686c97881af7984bc71c059f8
fastsly <szalai.laszlo.istvan.psiho@gmail.com>
2020-10-29 11:19:39 +0200

    corrected database connection

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "",
-    database: "alsterdorf",
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
 });
 // CREATE TABLE beneficiari (
